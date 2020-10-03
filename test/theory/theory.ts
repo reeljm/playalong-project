a58bd4f125028decb849dac4045ba0539fdf88cd
Jonah Mark Reel <jonahreel97@gmail.com>
2020-10-03 18:58:50 -0400

    fully covered theory distanceTo function

--- test/theory/theory.ts
+++ test/theory/theory.ts
@@ -1,14 +1,62 @@
 import * as assert from "assert";
 import { Theory } from "../../src/playbackService/theory/theory";
+import { Note } from "../../src/playbackService/theory/note";
 
 describe('Theory', function () {
+    
+    describe('#distanceTo(note1, note2)', function () {
+
+        const t: Theory = new Theory();
+        const testDistance = (note1: Note, note2: Note, expected: number) => {
+            it(`${note1.toPlayableString()} to ${note2.toPlayableString()} => ${expected}`, () => {
+                const result: number = t.distanceTo(note1, note2)
+                assert.equal(result, expected);
+            });
+        };
+
+        describe("distances from low to high are represented as positive numbers", () => {
+            let note1 = t.getNote("C", 5);
+            let note2 = t.getNote("G", 5);
+            testDistance(note1, note2, 7);
+
+            note1 = t.getNote("C", 5);
+            note2 = t.getNote("C", 6);
+            testDistance(note1, note2, 12);
+
+            note1 = t.getNote("C", 5);
+            note2 = t.getNote("C#", 5);
+            testDistance(note1, note2, 1);
+        });
+
+        describe("distances from high to low are represented as negative numbers", () => {
+            let note1 = t.getNote("G", 5);
+            let note2 = t.getNote("C", 5);
+            testDistance(note1, note2, -7);
+
+            note1 = t.getNote("C", 6);
+            note2 = t.getNote("C", 5);
+            testDistance(note1, note2, -12);
+
+            note1 = t.getNote("C#", 5);
+            note2 = t.getNote("C", 5);
+            testDistance(note1, note2, -1);
+        });
+
+        describe("distance between a note and itself is 0", () => {
+            let note1 = t.getNote("C", 5);
+            let note2 = t.getNote("C", 5);
+            testDistance(note1, note2, 0);
+        });
+    });
+
+
     describe('#parseEnharmonicPitch()', function () {
 
         const testEnharmonic = (input: string, expected: string) => {
             it(`${input} => ${expected}`, () => {
                 const t: Theory = new Theory();
                 const result: string = t.parseEnharmonicPitch(input);
-                assert.equal(result, expected)
+                assert.equal(result, expected);
             });
         };
         describe("Natural notes", () => {
