0afd7bdfe21b3c108f6b1d9efc189f9e68473489
Jonah Mark Reel <jonahreel97@gmail.com>
2020-11-03 01:20:28 -0500

    updated instrument config files to query samples from the right location

--- src/server.js
+++ src/server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
-app.use(express.static('.'));
+app.use(express.static('./dist'));
 const cors = require('cors');
 app.use(cors());
 
 app.listen(3001, async () => {
     app.get("/", (req, res) => {
-        res.sendFile("index.html", {root: "."});
+        res.sendFile("./dist/index.html", {root: "."});
     });
 });
