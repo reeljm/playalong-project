492e70d96dc985bde5d20fd65d7242c0c832b232
Jonah Reel <jonahreel97@gmail.com>
2020-11-18 06:04:46 +0000

    removed https stuff from server.js

--- src/server.js
+++ src/server.js
@@ -3,20 +3,10 @@ const app = express();
 app.use(express.static('./dist'));
 const cors = require('cors');
 app.use(cors());
-const fs = require('fs');
-const https = require('https');
 
-var privateKey = fs.readFileSync('./keys/key.pem');
-var certificate = fs.readFileSync('./keys/cert.pem');
-
-
-https.createServer({
-    key: privateKey,
-    cert: certificate
-}, app).listen(3001);
-
-
-app.get("/", (req, res) => {
-    res.sendFile("./dist/index.html", {root: "."});
+app.listen(3001, async () => {
+    app.get("/", (req, res) => {
+        res.sendFile("./dist/index.html", {root: "."});
+    });
 });
 
